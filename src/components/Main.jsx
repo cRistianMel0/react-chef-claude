e2550d60b35684e42a102b8364355a9d3a133b2a
cRistianMel0 <cRistianMel0@users.noreply.github.com>
2025-02-09 19:24:50 -0500

    Add useState to manage adding a single ingredient to the list.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
+
 function Main() {
+    const [ingredients, setNewIngredient] = useState([]);
+
+    const ingredientsList = ingredients.map((ingredient, index) => {
+        return <li key={index}>{ingredient}</li>
+    })
+
+    function submitHandler(event) {
+        event.preventDefault();
+        console.log('submitting form');
+        const form = new FormData(event.target);
+        let newIngredient = form.get('ingredient');
+        setNewIngredient(prevIngredients => [
+            ...prevIngredients,
+            newIngredient]);
+    }
+
     return (
         <main>
-            <form action="" className="recipe-form">
+            <form action="" onSubmit={submitHandler} className="recipe-form">
                 <input type="text"
                     placeholder="e.g. 1 cup of flour"
-                    aria-label="Add ingredient" />
-                <button>Add Ingredient</button>
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+
+                <button type="submit">Add Ingredient</button>
             </form>
+            <ol>
+                {ingredientsList}
+            </ol>
+
         </main>
     )
 }
